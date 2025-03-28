fb765ffe625415cb74a6e3b3c0ee1b54aad0bae4
Dimas Maulana Ahmad <97804178+dimasmaulana-ach@users.noreply.github.com>
2025-03-28 12:40:26 +0700

    landing

--- src/router/auth.tsx
+++ src/router/auth.tsx
@@ -1,13 +1,31 @@
 import Loaders from "@/components/loading/loaders";
 import DashboardLayouts from "@/layouts/dashboard";
+import LandingLayouts from "@/layouts/landing";
 import React from "react";
 import { RouteObject } from "react-router-dom";
 
 const DashboardPages = React.lazy(
   () => import("@/features/dashboard/pages/dashboard")
 );
+const LandingPages = React.lazy(
+  () => import("@/features/landing/pages/landing")
+);
 
 export const route_auth: RouteObject[] = [
+  {
+    path: "/",
+    element: <LandingLayouts />,
+    children: [
+      {
+        path: "",
+        element: (
+          <React.Suspense fallback={<Loaders isFullScreen />}>
+            <LandingPages />
+          </React.Suspense>
+        ),
+      },
+    ],
+  },
   {
     path: "/dashboard",
     element: <DashboardLayouts />,
